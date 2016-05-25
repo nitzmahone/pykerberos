095995a8f1adf8d4a71eb60111701ace0d3dbcd5
nitzmahone <mdavis@ansible.com>
2016-05-25 13:02:37 -0700

    version tweak

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ if krb5_ver:
 
 setup (
     name = "pykerberos",
-    version = "1.1.10",
+    version = "1.1.10-nitzmahone-principal-hack",
     description = "High-level interface to Kerberos",
     long_description=long_description,
     license="ASL 2.0",
